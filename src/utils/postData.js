2af751483fcce92f04063cbcdab4fe8f389ee9e9
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-11 21:06:57 -0600

    update postData with tech arrays

--- src/utils/postData.js
+++ src/utils/postData.js
@@ -6,7 +6,7 @@ export const Data = [
         description: 'Application allows users to search and view stock information. Users are able to organize and save stocks into watchlists.',
         repo: 'https://github.com/E-Huynh/stock-shares-viewer',
         link: 'https://young-everglades-16655.herokuapp.com/',
-        tech: ['Bulma', 'MySQL', 'Sequelize', 'Express', 'AJAX/Axios']
+        tech: ['Bulma', 'MySQL', 'Sequelize', 'Express', 'AJAX/Axios', 'IEXcloud']
     },
     {
         id: 'burger-wishlist',
@@ -14,7 +14,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/burger-wishlist.png?raw=true',
         description: 'Application that stores and displays users burger wishlist. The client displays and allows the user to interact with the database.',
         repo: 'https://github.com/E-Huynh/burger-wishlist',
-        link: 'https://young-everglades-16655.herokuapp.com/'
+        link: 'https://young-everglades-16655.herokuapp.com/',
+        tech: ['ExpressHandlebars', 'MVC', 'MySQL', 'ORM']
     },
     {
         id: 'employee-tracker',
@@ -22,7 +23,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/Employee%20tracker.png?raw=true',
         description: 'CLI application that lets users store, add, delete, and update employee information from a mysql database.',
         repo: 'https://github.com/E-Huynh/employee_tracker',
-        link: ''
+        link: '',
+        tech: ['Express', 'MySQL', 'Inquirer']
     },
     {
         id: 'note-taker',
@@ -30,7 +32,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/Note_taker.png?raw=true',
         description: 'Application that allows users to store, view and delete notes.',
         repo: 'https://github.com/E-Huynh/note_taker',
-        link: 'https://secure-waters-63965.herokuapp.com/'
+        link: 'https://secure-waters-63965.herokuapp.com/',
+        tech: ['#Bootstrap', 'FileSystem', 'Path', 'Jquery']
     },
     {
         id: 'team-profile-generator',
@@ -38,7 +41,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/teamProfile%20-%207%20members.png?raw=true',
         description: 'Application that use CLI to input employee information. HTML file automatically generated display team info.',
         repo: 'https://github.com/E-Huynh/Team_Profile_Generator',
-        link: ''
+        link: '',
+        tech: ['Bulma', 'Node.js', 'FileSystem', 'Inquirer']
     },
     {
         id: 'github-pdf-generator',
@@ -46,7 +50,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/github-pdf.png?raw=true',
         description: 'Application scrapes github API by username search to generate a PDF resume.',
         repo: 'https://github.com/E-Huynh/github-pdf',
-        link: ''
+        link: '',
+        tech: ['Axios', 'Puppeteer', 'Inquirer']
     },
     {
         id: 'fast-hikes',
@@ -54,7 +59,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/fast_hikes.png?raw=true',
         description: 'Application takes in driving duration and hike length from current location and returns hikes nearby that meet the search criteria.',
         repo: 'https://github.com/E-Huynh/FastHikes',
-        link: 'https://e-huynh.github.io/FastHikes/'
+        link: 'https://e-huynh.github.io/FastHikes/',
+        tech: ['CssGrid', 'Flexbox', 'Jquery', 'HikingProjectAPI', 'GoogleDistanceAPI']
     },
     {
         id: 'weather-dashboard',
@@ -62,7 +68,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/weather-dashboard.png?raw=true',
         description: 'Be prepared for whatever the weather throws at you. Allows the user to search for the current and 5-day forecast of any city.',
         repo: 'https://github.com/E-Huynh/weather_dashboard',
-        link: 'https://e-huynh.github.io/weather_dashboard/index.html'
+        link: 'https://e-huynh.github.io/weather_dashboard/index.html',
+        tech: ['CssGrid', 'Flexbox', 'LocalStorage', 'Moment.js', 'OpenWeatherAPI']
     },
     {
         id: 'work-day-scheduler',
@@ -70,7 +77,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/work_day_scheduler.png?raw=true',
         description: 'Track your daily schedule. Users can input their schedule by hour to plan out their day.',
         repo: 'https://github.com/E-Huynh/work-day-scheduler',
-        link: 'https://e-huynh.github.io/work-day-scheduler/'
+        link: 'https://e-huynh.github.io/work-day-scheduler/',
+        tech: ['HTML', 'CSS', 'Jquery']
     },
     {
         id: 'quiz-game',
@@ -78,7 +86,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/quiz-game.png?raw=true',
         description: 'Test your knowledge with this programming related interactice quiz with timer and a highscore tracker.',
         repo: 'https://github.com/E-Huynh/quiz_game',
-        link: 'https://e-huynh.github.io/quiz_game/quiz_game.html'
+        link: 'https://e-huynh.github.io/quiz_game/quiz_game.html',
+        tech: ['DOM', 'Javascript', 'CSS']
     },
     {
         id: 'password-generator',
@@ -86,7 +95,8 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/password-generator.png?raw=true',
         description: 'Quickly generate passwords with user specified inputs of character type and length.',
         repo: 'https://github.com/E-Huynh/Password_Generator',
-        link: 'https://e-huynh.github.io/Password_Generator/'
+        link: 'https://e-huynh.github.io/Password_Generator/',
+        tech: ['#Bootstrap', 'Javascript']
     },
     {
         id: 'rgb-game',
@@ -94,6 +104,7 @@ export const Data = [
         image: 'https://github.com/E-Huynh/Portfolio/blob/master/src/assets/rgb_game.png?raw=true',
         description: 'Test your knowledge of RGB color values by choosing the right tile. Easy and hard modes available.',
         repo: 'https://github.com/E-Huynh/RGB_game',
-        link: 'https://e-huynh.github.io/RGB_game/'
+        link: 'https://e-huynh.github.io/RGB_game/',
+        tech: ['HTML', 'CSS', 'Jquery']
     },
 ]
