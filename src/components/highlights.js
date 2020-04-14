49c92850b1452290e4d93fe907d788dd641f0b5e
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-14 17:46:49 -0600

    built highlights component

--- src/components/highlights.js
+++ src/components/highlights.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, Button } from '@material-ui/core';
+// import SaveIcon from '@material-ui/icons/Save';
+import Avatar from '@material-ui/core/Avatar';
 
 function Highlights() {
     return (
         <Card>
-            Contents go here
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+                startIcon={<Avatar src='https://avatars0.githubusercontent.com/u/56613046?s=460&u=dd342c337117a7ef40ee31b67df2c0b4c310f2cc&v=4'></Avatar>}
+            >
+                Who Am I?
+            </Button>
         </Card>
     )
 }
