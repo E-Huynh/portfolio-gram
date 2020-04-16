ed2e5a148e2563be7a4160e5fa5bc3b20b2de5f9
Erik Huynh <Erik.W.Huynh@Gmail.com>
2020-04-16 09:38:21 -0600

    Buttons filter posts by category
    Still need to handle 'display all posts'

--- src/components/highlights.js
+++ src/components/highlights.js
@@ -4,17 +4,20 @@ import { Card, Button, Typography } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 
 function Highlights({ data, setState }) {
-    function handleFilter(newValue) {
-        setState({postType: 'bio'})
+    function handleFilter() {
+        setState({postType: data.filterBy})
     }
 
     return (
-        <Card style={{borderRadius: 0}}>
+        <Card style={{borderRadius: 0}}
+            onClick={handleFilter}
+        >
             <Button
                 size="medium"
                 fullWidth
-                startIcon={<Avatar src={data.avatar}></Avatar>}
-                onClick={handleFilter}
+                startIcon={
+                <Avatar src={data.avatar}/>}
+
             >
                 <Typography
                     variant="body2"
--- src/utils/highlightsData.js
+++ src/utils/highlightsData.js
@@ -1,5 +1,11 @@
 export const HighlightsData = [
     {
+        id: 'all-posts',
+        text: 'All My Posts',
+        avatar: 'https://avatars0.githubusercontent.com/u/56613046?s=460&u=dd342c337117a7ef40ee31b67df2c0b4c310f2cc&v=4',
+        filterBy: 'all'
+    },
+    {
         id: 'about-me',
         text: 'Learn More About Who I Am',
         avatar: 'https://avatars0.githubusercontent.com/u/56613046?s=460&u=dd342c337117a7ef40ee31b67df2c0b4c310f2cc&v=4',
