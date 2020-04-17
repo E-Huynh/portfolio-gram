8b521b398370778c82c1e4fc43aae14786bc4aa2
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-17 12:45:09 -0600

    added 2 bio posts and new images

--- src/utils/postData.js
+++ src/utils/postData.js
@@ -11,6 +11,28 @@ export const Data = [
         type: 'bio'
     },
     {
+        id: 'education',
+        title: 'Education',
+        image: 'https://via.placeholder.com/750x520.png',
+        description: "After graduating from UVU in 2013 with a B.S. in Biotechnology, I worked as a Research Associate for 7 yrs in medical diagnostics. I was introduced to software development during that time and decide to enroll in the University of Utah coding bootcamp.",
+        repo: 'https://github.com/E-Huynh',
+        linkedin: 'https://www.linkedin.com/in/erik-huynh-228321196',
+        link: '',
+        tech: ['UtahValleyUniversity', 'UofU', 'Bootcamp', "Bachelor's Degree"],
+        type: 'bio'
+    },
+    {
+        id: 'personal',
+        title: 'Hobbies and Interests',
+        image: 'https://via.placeholder.com/750x520.png',
+        description: "When my schedule slows down, I enjoy many different hobbies including: fishing, painting miniatures, watching the NHL, and traveling to new places. During my recent travels, I had to opportunity to stop a Crater Lake National Park, Redwood National Forest, and Portland, OR. Portland was where I caught the largest fish of my life! A 6' 250 lbs sturgeon from the Columbia River.",
+        repo: 'https://github.com/E-Huynh',
+        linkedin: 'https://www.linkedin.com/in/erik-huynh-228321196',
+        link: '',
+        tech: ['UtahValleyUniversity', 'UofU', 'Bootcamp', "Bachelor's Degree"],
+        type: 'bio'
+    },
+    {
         id: 'stock-watcher',
         title: 'Stock Watcher',
         image: 'https://github.com/E-Huynh/portfolio-gram/blob/master/src/assets/stock-watcher.png?raw=true',
